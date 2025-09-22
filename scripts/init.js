96cbf373a86827da7197f9f94a58ab267af8a6f3
BooksNTomes <66352480+BooksNTomes@users.noreply.github.com>
2025-09-22 14:26:54 +0800

    another folderstructure adjustment, will proceed to script overhauls in the next commit

---
+++ scripts/init.js
@@ -0,0 +1,70 @@
+/** @param {NS} ns */
+export async function main(ns) {
+
+    const bitnode = ns.args[0] !== null ? ns.args[0] : 1;
+
+    switch (bitnode){
+        case 1:
+            // run init-hack.js
+            // for bitnode 1 / hack focused bitnodes
+            break;
+        case 2:
+            // run init-gangs.js
+            // for bitnode 2 / gang focused bitnodes
+            break;
+        case 3: 
+            // run init-corporations.js
+            // for bitnode 3 / corporation focused bitnodes
+            break;
+        case 4:
+            // run init-singularity.js
+            // for bitnode 4 / singularity focused bitnodes
+            break;
+        case 5:
+            // run init-ai.js
+            // for bitnode 5 / ai focused bitnodes
+            break;
+        case 6:
+            // run init-bladeburners.js
+            // for bitnode 6 / bladeburner focused bitnodes
+            break;
+        case 7:
+            // run init-2079.js
+            // for bitnode 7 / bladeburner 2079 focused bitnodes
+            break;
+        case 8:
+            // run init-stockmarket.js
+            // for bitnode 8 / stockmarket focused bitnodes
+            break;
+        case 9:
+            // run init-hacknet.js
+            // for bitnode 9 / hacknet focused bitnodes
+            break;
+        case 10:
+            // run init-digitalcarbon.js
+            // for bitnode 10 / digital carbon
+            break;
+        case 11:
+            // run init-bigcrash.js
+            // for bitnode 11 / the big crash
+            break;
+        case 12:
+            // run init-recursion.js
+            // for bitnode 12 / recursion
+            break;
+        case 13:
+            // run init-lunatics.js
+            // for bitnode 13 / they are all lunatics
+            break;
+        case 14:
+            // run init-ipvgo.js
+            // for bitnode 14 / ipvgonet focused bitnodes
+            break;
+        default:
+            // do nothing
+            break;
+    }
+
+    // run utility scripts
+
+}
---
+++ scripts/init/gangs.js
@@ -0,0 +1,5 @@
+/** @param {NS} ns */
+export async function main(ns) {
+    /** Script starter after augmentation / bitnode reset */
+    gangs = ns.gang;
+}
