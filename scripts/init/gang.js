4454e709972325f5e8fbc0d3165d6aba5fdcaf3b
BooksNTomes <66352480+BooksNTomes@users.noreply.github.com>
2025-09-29 17:38:44 +0800

    starting some gang scripts

--- scripts/init/gang.js
+++ scripts/init/gang.js
@@ -1,7 +1,12 @@
 /** @param {NS} ns */
 export async function main(ns) {
     /** Script starter after augmentation / bitnode reset */
-    gangs = ns.gang;
+    const gang = ns.gang;
+    const faction = ns.args[0];
 
-    
+    try{
+        gang.createGang(faction)
+    } catch (error) {
+        ns.tprintf(`Error in creating gang: ${error}`);
+    }
 }
---
+++ scripts/utils/stats/gangStats.js
@@ -0,0 +1,24 @@
+/** @param {NS} ns */
+export async function main(ns) {
+    const gang = ns.gang;
+
+    console.log('getBonusTime()', gang.getBonusTime()); 
+    // 	Get bonus time.
+
+    console.log('getEquipmentNames()', gang.getEquipmentNames()); 
+    // 	List equipment names.
+
+    console.log('getGangInformation()', gang.getGangInformation()); 
+    // 	Get information about your gang.
+
+    console.log('getMemberNames()', gang.getMemberNames()); 
+    // 	List all gang members.
+
+    console.log('getOtherGangInformation()', gang.getOtherGangInformation()) 
+    // 	Get information about the other gangs.
+
+    console.log('getRecruitsAvailable()', gang.getRecruitsAvailable()); 
+    // 	Check how many gang members you can currently recruit.
+}
+
+
