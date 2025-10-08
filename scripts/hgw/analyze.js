15a8169977d1bb3a62f984a512861dd529b4c104
BooksNTomes <66352480+BooksNTomes@users.noreply.github.com>
2025-10-08 16:54:16 +0800

    analyze.js is ready for testing

--- scripts/hgw/analyze.js
+++ scripts/hgw/analyze.js
@@ -2,20 +2,44 @@
 export async function main(ns) {
 
     /** Experimental Script For Analyzing Server Optimal Thread Assignments */
+    let tempTarget = 'n00dles';
+    let cores = 1;
+    console.log(`Target is ${tempTarget}`);
+    
+    /** Analysis functions
+     * hackAnalyze(host) 	                    Get the part of money stolen with a single thread.
+     * hackAnalyzeChance(host) 	                Get the chance of successfully hacking a server.
+     * hackAnalyzeSecurity(threads, hostname) 	Get the security increase for a number of threads.
+     * hackAnalyzeThreads(host, hackAmount) 	Calculate the decimal number of threads needed to hack a specified amount of money from a target host.
+     * growthAnalyze(host, multiplier, cores) 	Calculate the number of grow threads needed for a given multiplicative growth factor.
+     * growthAnalyzeSecurity(threads, hostname, cores) 	Calculate the security increase for a number of grow threads.
+     * weakenAnalyze(threads, cores) 	Predict the effect of weaken.
+    */
 
-    const host = ns.args[0];
-    const growthMultiplier = .5;
-    const growthSecurity = 0;
-    const hackMoney = 0;
+    /** Hack Thread Experiment 
+    * - Check if hackTotal == actualHackingMoneyGain
+    */
+    let hackThreads = 5;
+    let hackTotal = 0;
+    for (let i = 0; i < hackThreads; i++){
+        hackTotal += hackAnalyze(tempTarget);
+    }
+    console.log(`${hackThreads} hack threads results in ${hackTotal}`)
+    console.log(`checking with hackAnalyzeThreads: ${ns.hackAnalyzeThreads(tempTarget, hackTotal)}`);
 
-    // growthAnalyze(host, multiplier, cores) 	Calculate the number of grow threads needed for a given multiplicative growth factor.
+    /** Grow Thread Experiment 
+     * - Check for effects of a given multiplicative growth factor
+    */
+    let multiplier = 0.2;
+    let growTotal = 0;
+    let targetMoney = ns.getMoneyAvailable(tempTarget);
+    growTotal = targetMoney + (targetMoney * multiplier);
+    let growThreads = ns.growthAnalyze(tempTarget, multiplier, cores);
+    console.log(`${growThreads} grow threads results in ${growTotal}`);
     
-    // growthAnalyzeSecurity(threads, hostname, cores) 	Calculate the security increase for a number of grow threads.
-    
-    // hackAnalyze(host) 	Get the part of money stolen with a single thread.
-    // hackAnalyzeChance(host) 	Get the chance of successfully hacking a server.
-    // hackAnalyzeSecurity(threads, hostname) 	Get the security increase for a number of threads.
-    // hackAnalyzeThreads(host, hackAmount) 	Calculate the decimal number of threads needed to hack a specified amount of money from a target host.
-    
-    // weakenAnalyze(threads, cores) 	Predict the effect of weaken.
+    /** Weaken Threads Experiment 
+     * - Check for power of weakenat 1 core
+    */
+   let weakenThreads = 20;
+   console.log(`weaken with ${weakenThreads} results in ${ns.weakenAnalyze(weakenThreads, cores)}`)
 }
